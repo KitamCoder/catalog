2bb4473937437d84152f36f940b9ee1e5eb97ea7
Kitam <whitehacker638@gmail.com>
2020-08-01 16:39:35 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$('#go_top').click(function() {
+	$('html, body').animate({scrollTop: 0},500);
+	return false;
+});
+
+$('.toggle_mobile_nav').click(function() {
+	if ($('.mobile_nav').is(':visible')) {
+		$('.mobile_nav').fadeOut(150);
+	}
+	else {
+		$('.mobile_nav').fadeTo(150, 0.95);
+	}
+});
+
+$('.toggle_searching_menu').click(function() {
+	if ($('.searching_menu').is(':visible')) {
+		$('.searching_menu').fadeOut(150);
+	}
+	else {
+		$('.searching_menu').fadeTo(150, 0.95);
+	}
+});
